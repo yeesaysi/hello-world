067b13ec15efe500865d1ca3d0702d4836063b33
yeesaysi <stahlerh@gmail.com>
2016-02-26 12:09:24 -0500

    Added files via upload

---
+++ layout.py
@@ -0,0 +1,18 @@
+def header():
+	header = """
+	This python script will convert units based on your input.
+	Currently, we can convert the following units:
+	"""
+	print(header)
+
+def list_of_conversions():
+	list_of_conversions = ("""
+	\t1 Inches to feet
+	\t2 Feet to inches
+	\t3 Miles to kilometers
+	\t4 Kilometers to miles
+	Please type the number of the conversion you would like to perform and
+	press ENTER
+	""")
+
+	print(list_of_conversions)
